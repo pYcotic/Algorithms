07c1aed072c97f3404f37a6fd057cb01e4d89de6
Marco Slabbert <60457007+pYcotic@users.noreply.github.com>
2023-05-04 09:30:46 +0700

    Added euclids algorithm in python

--- Euclids_GCD/python/euclids_algorithm.py
+++ Euclids_GCD/python/euclids_algorithm.py
@@ -1,9 +1,9 @@
-def euclid(a, b):
+def euclid_gcd(a, b):
     if a == b:
         return a
     elif a > b:
         a -= b
-        return euclid(a, b)
+        return euclid_gcd(a, b)
     elif a < b:
         b -= a
-        return euclid(a, b)
+        return euclid_gcd(a, b)
